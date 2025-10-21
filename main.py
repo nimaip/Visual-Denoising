5a17301dca0023a6ee3b72d7511d101a32dc2f5c
Nimai Ponna <nimaipponna@gmail.com>
2025-10-20 22:10:31 -0400

    Created Medical Image Dataset Class

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
+
+class MedicalImageDataset(Dataset):
+    def __init__(self, image_dir, transform=None):
+        self.image_dir = image_dir
+        self.image_files = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.lower().endswith('.tif')]
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        image_path = self.image_files[idx]
+        image = Image.open(image_path).convert('L')
+
+        if self.transform:
+            image = self.transform(image)
+        return image
